6274c6a8ee3501fa8ea542ded9695a6a0b06b73f
mwardynski <mwardynski@gmail.com>
2013-07-10 19:53:27 +0200

    Setting Up the Example for the Chapter

--- src/main/java/com/apress/pss/terrormovies/controller/AdminController.java
+++ src/main/java/com/apress/pss/terrormovies/controller/AdminController.java
@@ -1,29 +1,23 @@
 package com.apress.pss.terrormovies.controller;
 
-import org.springframework.security.core.context.SecurityContextHolder;
-import com.apress.pss.terrormovies.model.User;
-import org.springframework.stereotype.Controller;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
+
 @RequestMapping("/admin")
-public class AdminController {
+public interface AdminController {
 
     @RequestMapping(method = RequestMethod.POST, value = "/movies")
     @ResponseBody
-    public String createMovie(@RequestBody String movie) {
-        System.out.println("Adding movie!! " + movie);
-        return "created";
-    }
+    @Secured("ROLE_ADMIN")
+    public abstract String createMovie(@RequestBody String movie);
 
     @RequestMapping(method = RequestMethod.GET, value = "/movies")
     @ResponseBody
-    public String createMovie() {
-        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        System.out.println("returned movie!");
-        return "User " + user.getLastname() + " is accessing movie x";
-    }
+    @Secured("ROLE_ADMIN")
+    public abstract String createMovie();
+
 }
