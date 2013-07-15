3b175b8aad6078d3e60bbc7fb8932204fcf2ce0f
mwardynski <mwardynski@gmail.com>
2013-07-15 23:06:31 +0200

    Securing the Data Returned from a Method

--- src/main/java/com/apress/pss/terrormovies/service/MoviesServiceImpl.java
+++ src/main/java/com/apress/pss/terrormovies/service/MoviesServiceImpl.java
@@ -1,7 +1,7 @@
 package com.apress.pss.terrormovies.service;
 
 import com.apress.pss.terrormovies.model.Movie;
-import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.access.prepost.PostAuthorize;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -12,9 +12,10 @@ public class MoviesServiceImpl implements MoviesService {
 
     static {
         MOVIES.put("die hard", new Movie("Die Hard", "20000000"));
+        MOVIES.put("two days in paris", new Movie("two days in paris ", "1000000"));
     }
 
-    @PreAuthorize("#name.length() < 50 and hasRole('ROLE_USER')")
+    @PostAuthorize("T(java.lang.Integer).parseInt(getReturnObject().budget) < 5000000")
     public Movie getMovieByName(String name) {
         return MOVIES.get(name.toLowerCase());
     }
