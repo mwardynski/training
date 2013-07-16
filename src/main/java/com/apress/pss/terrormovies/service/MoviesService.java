0620f1cedffeeaae07239e1cba39511c4b21942d
mwardynski <mwardynski@gmail.com>
2013-07-16 22:28:09 +0200

    Filtering Collections Sent and Returned from Methods

--- src/main/java/com/apress/pss/terrormovies/service/MoviesService.java
+++ src/main/java/com/apress/pss/terrormovies/service/MoviesService.java
@@ -2,6 +2,13 @@ package com.apress.pss.terrormovies.service;
 
 import com.apress.pss.terrormovies.model.Movie;
 
+import java.util.Collection;
+import java.util.List;
+
 public interface MoviesService {
     Movie getMovieByName(String name);
+
+    void addNewMovies(List<String> movies);
+
+    Collection<Movie> getAllMovies();
 }
