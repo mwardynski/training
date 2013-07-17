01445aefdb5aa89ba41976a184f6ead147ed956d
mwardynski <mwardynski@gmail.com>
2013-07-18 00:09:20 +0200

    Security Without a Web Layer

---
+++ src/main/java/com/apress/pss/terrormovies/access/AccessOperations.java
@@ -0,0 +1,9 @@
+package com.apress.pss.terrormovies.access;
+
+public interface AccessOperations {
+    void login(String username, String password);
+
+    void logout();
+
+    String executeOperation(String beanName, String method);
+}
